1ffe620ea1386adef7246f972608c4a15194e022
jachapin <32234320+jachapin@users.noreply.github.com>
2020-11-05 00:46:30 -0600

    Update DaysToHoliday.py
    Program works

--- DaysToHoliday.py
+++ DaysToHoliday.py
@@ -1,28 +1,35 @@
-from datetime import datetime
+from datetime import date
+
+# This program counts down the days until Christmas or New Year's. User selects which holiday.
 
 def main():
-    chooseHoliday()
-    print(holiday)
+
+    print("Let's count down the days until your favorite holiday! Enter 1 for Christmas or 2 for New Year\'s")
+    holiday = input()
+
     if holiday == "1":
         daysTillChristmas()
     else:
-        print("New Year's")
+        daysTillNewYear()
 
-# User chooses holiday
-def chooseHoliday():
-    print("Let's count down the days until your favorite holiday! Enter 1 for Christmas or 2 for New Year\'s")
-    global holiday
-    holiday = input()
 
-# Calculate days until Christmas. Return number of days until Christmas
+# Calculate days until Christmas. 
 def daysTillChristmas():
-    today = datetime.now()
-    print("The current date and time is ", today)
+    today = date.today()
+    print("The current date and time is", today)
     christmasDay = date(today.year, 12, 25)
     if christmasDay < today:
         christmasDay = christmasDay.replace(year = today.year + 1)
     daysUntilChristmas = christmasDay - today
-    print("It is ", daysUntilChristmas, " days until Christmas!")
+    print("It is", daysUntilChristmas.days, "days until Christmas!")
+
+# Calculate the number of days until New Year's Day. 
+def daysTillNewYear():
+    today = date.today()
+    print("The current date and time is", today)
+    newYearsDay = date(today.year + 1, 1, 1)
+    daysUntilNewYears = newYearsDay - today
+    print("It is", daysUntilNewYears.days, "days until New Year's!")
 
 if __name__ == "__main__":
     main()
